ba329198426b41d98d6099f1d0e7cc3bdf08ecaf
iqubal <iqubal.ramzid@outlook.fr>
2022-04-11 10:00:38 +0200

    first commit

---
+++ src/Entity/Employes.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EmployesRepository::class)
+ */
+class Employes
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
